54ba1193153ff6eacd2fc43979d1e71d11a86872
Jim Fitzpatrick <fitzpatrick.jim@gmail.com>
2012-04-12 11:52:46 -0400

    Add theme stuff from zshrc.

--- custom/theme-and-appearance.zsh
+++ custom/theme-and-appearance.zsh
@@ -1,6 +1,8 @@
 source $HOME/.oh-my-zsh/lib/theme-and-appearance.zsh
 unsetopt cdablevars
 setopt NOCDABLEVARS
+setopt nohup
+setopt NO_beep
 
 if [[ "$TERM" != "dumb" ]]; then
     if [[ "`uname`" = Darwin ]]; then
@@ -17,3 +19,18 @@ export LESS_TERMCAP_se=$'\E[0m'
 export LESS_TERMCAP_so=$'\E[01;44;33m'
 export LESS_TERMCAP_ue=$'\E[0m'
 export LESS_TERMCAP_us=$'\E[01;32m'
+
+function theme_command_precmd {
+    # Output sudo commands in red
+    if [[ "${1[0,4]}" = sudo ]]; then
+        echo -ne '\e[0;31m'
+    # Prepend ack with a purple bar
+    elif [[ "${1[0,3]}" = ack ]]; then
+        echo -ne '\e[0;35m'
+        printf "%$(tput cols)s\n"|tr ' ' '─'
+        echo -ne '\e[0m'
+    fi
+}
+
+autoload -U add-zsh-hook
+add-zsh-hook precmd theme_command_precmd
